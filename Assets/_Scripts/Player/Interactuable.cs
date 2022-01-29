572dae39c052011cadee00a755b2638e24d2f004
Alam Rodriguez <alamgrodriguez@gmail.com>
2022-01-29 12:26:54 -0600

    Interactuable objects logic
    Intereactuable.cs

---
+++ Assets/_Scripts/Player/Interactuable.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactuable : MonoBehaviour
+{
+    [SerializeField] protected bool interactionEnabled = true;
+    [Header("Interaction area attributes")]
+    [SerializeField] protected Transform interactionAreaPivot;
+    [SerializeField] protected float interactionAreaRadio;
+
+    private Collider2D[] overlapedColliders;
+    private bool readyToInteract = false;
+
+    private void Update()
+    {
+        overlapedColliders = Physics2D.OverlapCircleAll(interactionAreaPivot.position, interactionAreaRadio);
+        if(overlapedColliders.Length > 0)
+        {
+            foreach (Collider2D objCollider in overlapedColliders)
+            {
+                if (objCollider.CompareTag(K.Tag.player))
+                {
+                    readyToInteract = true;
+                }
+            }
+        }
+    }
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(K.Tag.player))
+        {
+            
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        if(interactionAreaPivot && interactionAreaRadio > 0f)
+        Gizmos.DrawWireSphere(interactionAreaPivot.position, interactionAreaRadio);
+    }
+
+    public void Interact()
+    {
+        //Logic to interact and perform required actions
+        if (interactionEnabled && readyToInteract)
+            print("HI HI!");
+
+    }
+
+    protected void Dialog()
+    {
+
+    }
+}
---
+++ Assets/_Scripts/Player/K.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class K
+{
+    public class Tag
+    {
+        public const string player = "Player";
+    }
+
+}
